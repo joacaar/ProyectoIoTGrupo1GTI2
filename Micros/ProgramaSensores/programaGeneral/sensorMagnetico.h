4f43af723a3764d041803458e3612d82d5b83fb7
Víctor White <36444816+vicblab1@users.noreply.github.com>
2018-11-04 12:40:30 +0100

    Corrección código sensor magnético

--- Micros/ProgramaSensores/programaGeneral/sensorMagnetico.h
+++ Micros/ProgramaSensores/programaGeneral/sensorMagnetico.h
@@ -8,11 +8,32 @@ JsonObject& envio = jsonBuffer.createObject(); //creación del objeto "envio"
 //Constantes y variables globales
 const int Pin = 5;
 bool puertaAbierta = false;
-
+char texto2[200];
 void configuracionPuerta() {
   // put your setup code here, to run once:
   Serial.begin(115200);
   pinMode(Pin, INPUT_PULLUP);
+  if (digitalRead(Pin) == LOW) {
+
+	  envio["Puerta"] = "Principal";
+	  envio["Estado"] = "Cerrada";
+	  puertaAbierta = false;
+	  envio.printTo(texto2);         //paso del objeto "envio" a texto para transmitirlo
+	  //udp.broadcastTo(texto, 1234); //se envía por el puerto 1234 el JSON como texto
+	  Serial.print("Enviando: ");
+	  Serial.println(texto2);
+	 
+
+  }
+  else if(digitalRead(Pin)== HIGH){
+	  envio["Puerta"] = "Principal";
+	  envio["Estado"] = "Abierta";
+	  puertaAbierta = true;
+	  envio.printTo(texto2);         //paso del objeto "envio" a texto para transmitirlo
+	  //udp.broadcastTo(texto, 1234); //se envía por el puerto 1234 el JSON como texto
+	  Serial.print("Enviando: ");
+	  Serial.println(texto2);
+  }
 }
 
 //PUERTA
@@ -23,7 +44,7 @@ void lecturaPuerta(int Pin){
   if (puertaAbierta) {
     if (digitalRead(Pin) == LOW) {
       delay(1000);
-      if (digitalRead(Pin) == LOW) {
+     
         envio["Puerta"] = "Principal";
         envio["Estado"] = "Cerrada";
         puertaAbierta = false;
@@ -32,13 +53,13 @@ void lecturaPuerta(int Pin){
         Serial.print("Enviando: ");
         Serial.println(texto);
         delay(200);
-      }
+      
     }
 
   } else {
     if (digitalRead(Pin) == HIGH) {
       delay(1000);
-      if (digitalRead(Pin) == HIGH) {
+    
         envio["Puerta"] = "Principal";
         envio["Estado"] = "Abierta";
         puertaAbierta = true;
