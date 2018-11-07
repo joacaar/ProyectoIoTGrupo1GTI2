8d1853aeedd8b50499bc4899f1e96c2776260930
Santi <santiperez1199@gmail.com>
2018-11-07 21:01:26 +0100

    GUARDAR DATOS BASE DE DATOS

--- RPI/RPIUART/app/src/main/java/com/proyectoiot/grupo1gti/rpi_uart/MainActivity.java
+++ RPI/RPIUART/app/src/main/java/com/proyectoiot/grupo1gti/rpi_uart/MainActivity.java
@@ -2,8 +2,14 @@ package com.proyectoiot.grupo1gti.rpi_uart;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.util.Log;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.HashMap;
@@ -19,6 +25,8 @@ public class MainActivity extends Activity {
 
         Log.i(TAG, "Lista de UART disponibles: " + ArduinoUart.disponibles());
         ArduinoUart uart = new ArduinoUart("UART0", 115200);
+
+        //A
         Log.d(TAG, "Mandado a Arduino: A");
         uart.escribir("A");
         try {
@@ -29,6 +37,8 @@ public class MainActivity extends Activity {
         String altura = uart.leer();
         Log.d(TAG, "Recibido de Arduino: "+altura);
 
+
+        //P
         Log.d(TAG, "Mandado a Arduino: P");
         uart.escribir("P");
         try {
@@ -37,14 +47,54 @@ public class MainActivity extends Activity {
             Log.w(TAG, "Error en sleep()", e);
         }
         String peso = uart.leer();
-        Log.d(TAG, "Recibido de Arduino: "+peso);
+        Log.d(TAG, "Recibido de Arduino: "+ peso);
 
+        //E
+        Log.d(TAG, "Mandado a Arduino: E");
+        uart.escribir("E");
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            Log.w(TAG, "Error en sleep()", e);
+        }
+        String puerta = uart.leer();
+        Log.d(TAG, "Recibido de Arduino: "+ puerta);
+
+        //F
+        Log.d(TAG, "Mandado a Arduino: F");
+        uart.escribir("F");
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            Log.w(TAG, "Error en sleep()", e);
+        }
+        String movimiento = uart.leer();
+        Log.d(TAG, "Recibido de Arduino: "+ movimiento);
 
         FirebaseFirestore db = FirebaseFirestore.getInstance();
-        Map<String, Object> datos = new HashMap<>();
-        datos.put("Altura", altura);
-        datos.put("Peso", peso);
-        db.collection("coleccion").document("documento").update(datos);
+       // Create a new user with a first and last name
+        Map<String, Object> user = new HashMap<>();
+        user.put("first", "Ada");
+        user.put("last", "Lovelace");
+        user.put("born", 1815);
+
+// Add a new document with a generated ID
+        db.collection("users")
+                .add(user)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error adding document", e);
+                    }
+                });
+
+
 
     }
 
