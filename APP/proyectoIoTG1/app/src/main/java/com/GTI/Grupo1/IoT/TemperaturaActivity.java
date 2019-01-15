51ab76faebf459049eaff73837c7830bac14509e
Santi <santiperez1199@gmail.com>
2019-01-15 23:31:35 +0100

    boton editar foto

--- APP/proyectoIoTG1/app/src/main/java/com/GTI/Grupo1/IoT/TemperaturaActivity.java
+++ APP/proyectoIoTG1/app/src/main/java/com/GTI/Grupo1/IoT/TemperaturaActivity.java
@@ -3,16 +3,33 @@ package com.GTI.Grupo1.IoT;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
+import android.view.View;
+import android.widget.TextView;
 
 
 public class TemperaturaActivity extends Activity {
+
+    private int numero = 20;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.temperatura);
 
+        TextView t = findViewById(R.id.textotemp);
+        t.setText(String.valueOf(numero));
 
     }
 
+    public void Sumar (View view){
+        TextView t = findViewById(R.id.textotemp);
+        numero++;
+        t.setText(String.valueOf(numero));
+    }
+    public void Restar (View view){
+        TextView t = findViewById(R.id.textotemp);
+        numero--;
+        t.setText(String.valueOf(numero));
+    }
 
 }
